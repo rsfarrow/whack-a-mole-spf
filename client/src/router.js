aba50fb918d8d5cf441189e055b7ca677a87f68a
Sam Farrow <sfarrow@bioiq.com>
2019-08-12 16:03:03 -0400

    Remove history mode from router, adds hash to URL

--- client/src/router.js
+++ client/src/router.js
@@ -6,7 +6,6 @@ Vue.use(Router)
 const Login = () => import('./views/login.vue')
 const Game = () => import('./views/whack-a-mole.vue')
 export default new Router({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     { path: '*',
